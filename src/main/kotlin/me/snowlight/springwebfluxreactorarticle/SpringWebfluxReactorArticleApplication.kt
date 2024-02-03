606d978312ed2055e2d881c33e67e6edcb6cf282
snowlight-aemt <it.s.aemt@gmail.com>
2024-02-03 21:54:44 +0900

    Init Project

---
+++ src/main/kotlin/me/snowlight/springwebfluxreactorarticle/SpringWebfluxReactorArticleApplication.kt
@@ -0,0 +1,11 @@
+package me.snowlight.springwebfluxreactorarticle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxReactorArticleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebfluxReactorArticleApplication>(*args)
+}
---
+++ src/test/kotlin/me/snowlight/springwebfluxreactorarticle/SpringWebfluxReactorArticleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.snowlight.springwebfluxreactorarticle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxReactorArticleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
